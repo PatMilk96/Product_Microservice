c1eb59f498b16deeaef677a23ac12baa62f6f7f4
Patryk Milkiewicz <g00397265@atu.ie>
2023-12-28 19:20:43 +0000

    Added ProductController code with a PostMapping to pass a Product to be added to the database

--- src/main/java/ie/atu/product_microservice/ProductController.java
+++ src/main/java/ie/atu/product_microservice/ProductController.java
@@ -1,4 +1,22 @@
 package ie.atu.product_microservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private RepositoryService repositoryService;
+    @Autowired
+    public ProductController(RepositoryService repositoryService){
+        this.repositoryService = repositoryService;
+    }
+
+    @PostMapping("/addProduct")
+    public String addProduct(@RequestBody ProductDetails productDetails){
+        String response = repositoryService.addProduct(productDetails);
+        return response;
+    }
 }
--- src/main/java/ie/atu/product_microservice/RepositoryService.java
+++ src/main/java/ie/atu/product_microservice/RepositoryService.java
@@ -1,4 +1,11 @@
 package ie.atu.product_microservice;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "Repository-Service", url = "http://localhost:8081")
 public interface RepositoryService {
+    @PostMapping("/add")
+    String addProduct(@RequestBody ProductDetails productDetails);
 }
