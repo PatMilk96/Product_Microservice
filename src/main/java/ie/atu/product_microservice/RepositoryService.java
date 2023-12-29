3bef358f030bb0e2a62a0efeaebda693f61c8599
Patryk Milkiewicz <g00397265@atu.ie>
2023-12-29 15:58:46 +0000

    Created a docker compose file and containerized my microservices so, they can communicate with each other

--- src/main/java/ie/atu/product_microservice/RepositoryService.java
+++ src/main/java/ie/atu/product_microservice/RepositoryService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "Repository-Service", url = "http://localhost:8081")
+@FeignClient(name = "Repository-Service", url = "http://Repository-Service:8081")
 public interface RepositoryService {
     @PostMapping("/add")
     String addProduct(@RequestBody ProductDetails productDetails);
