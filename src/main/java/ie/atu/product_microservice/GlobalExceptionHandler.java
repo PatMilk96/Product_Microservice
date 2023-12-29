3c13a4886d0188ba81a1f74c2fa705ee7060411d
Patryk Milkiewicz <g00397265@atu.ie>
2023-12-29 13:52:29 +0000

    Added Error Handling

---
+++ src/main/java/ie/atu/product_microservice/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package ie.atu.product_microservice;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErrorDetails> handleValidationException(MethodArgumentNotValidException ex){
+        List<ErrorDetails> errors = new ArrayList<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            ErrorDetails errorDetails = new ErrorDetails(fieldName, errorMessage);
+            errors.add(errorDetails);
+        });
+        return errors;
+    }
+}
